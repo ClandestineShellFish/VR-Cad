29edfea2c9178de1ab0abe31a108e3979f9b7d05
Unknown <jimbo@nicholl.org.uk>
2020-01-26 18:29:45 +0000

    example casting and drawing for Shaw

---
+++ Assets/DataManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataManager : MonoBehaviour
+{
+	private List<Vector3> points = new List<Vector3>();
+	[SerializeField] private LineRenderer lineRenderer;
+
+	public void OnHandTrigger(Vector3 point)
+	{
+		points.Add(point);
+		lineRenderer.positionCount = points.Count;
+		lineRenderer.SetPositions(points.ToArray());
+	}
+}
---
+++ Assets/TargetCaster.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetCaster : MonoBehaviour
+{
+	[SerializeField] private Transform origin;
+	[SerializeField] private LineRenderer lineRenderer;
+	[SerializeField] private OVRInput.Button button;
+	[SerializeField] private LayerMask layerMask;
+	[SerializeField] private DataManager manager;
+	private RaycastHit hit;
+
+	private Vector3 OffsetOrigin => origin.position + origin.transform.forward * 0.05F;
+
+	private void Update()
+	{
+		if (!Physics.Raycast(origin.position, origin.transform.forward, out hit, 100F, layerMask))
+		{
+			lineRenderer.SetPositions(new Vector3[] { OffsetOrigin, OffsetOrigin + 100F * origin.transform.forward });
+			return;
+		}
+
+		lineRenderer.SetPositions(new Vector3[] { OffsetOrigin, hit.point });
+		if (OVRInput.GetDown(button))
+		{
+			Debug.Log("Input: " + hit.point);
+			manager.OnHandTrigger(hit.point);
+		}
+	}
+}
